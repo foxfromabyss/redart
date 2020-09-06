8c425ba62f8b4e7d0f328be1b48a04747020618c
Sergei Gunstvin <foxfromabyss@gmail.com>
2020-09-06 20:40:19 +0200

    Add wss reader

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/foxfromabyss/redart
 
 go 1.15
 
-require github.com/joho/godotenv v1.3.0
+require (
+	github.com/gorilla/websocket v1.4.2
+	github.com/joho/godotenv v1.3.0
+)
--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
+	"net/url"
 	"os"
+	"os/signal"
+	"time"
 
+	"github.com/gorilla/websocket"
 	"github.com/joho/godotenv"
 )
 
@@ -13,6 +18,57 @@ var (
 	BitmexSecret string
 )
 
+var addr = flag.String("addr", "testnet.bitmex.com", "http service address")
+
+func listen() {
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+	u := url.URL{Scheme: "wss", Host: *addr, Path: "/realtime"}
+
+	log.Printf("connecting to %s", u.String())
+
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer c.Close()
+
+	done := make(chan struct{})
+
+	go func() {
+		defer close(done)
+		for {
+			_, message, err := c.ReadMessage()
+			if err != nil {
+				log.Println("read:", err)
+				return
+			}
+			log.Printf("recv: %s", message)
+		}
+	}()
+	for {
+		select {
+		// case <-done:
+		// 	return
+		case <-interrupt:
+			log.Println("interrupt")
+
+			// Cleanly close the connection by sending a close message and then
+			// waiting (with timeout) for the server to close the connection.
+			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+			if err != nil {
+				log.Println("write close:", err)
+				return
+			}
+			select {
+			case <-done:
+			case <-time.After(time.Second):
+			}
+			return
+		}
+	}
+
+}
 func init() {
 	err := godotenv.Load()
 	if err != nil {
@@ -21,8 +77,13 @@ func init() {
 	BitmexID = os.Getenv("BITMEX_ID")
 	BitmexSecret = os.Getenv("BITMEX_SECRET")
 }
-
 func main() {
 	fmt.Println(BitmexID)
 	fmt.Println(BitmexSecret)
+
+	// bitmex_secret := os.Getenv("BITMEX_SECRET")
+	// bitmex_id := os.Getenv("BITMEX_ID")
+	// fmt.Println(bitmex_secret)
+	// fmt.Println(bitmex_id)
+	listen()
 }
