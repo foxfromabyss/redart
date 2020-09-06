a13e2d965708e66a63bb3b94409078f4a597ecd3
Sergei Gunstvin <foxfromabyss@gmail.com>
2020-09-06 20:20:01 +0200

    Read API key from .env file

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	bitmex_secret := os.Getenv("BITMEX_SECRET")
+	bitmex_id := os.Getenv("BITMEX_ID")
+	fmt.Println(bitmex_secret)
+	fmt.Println(bitmex_id)
 }
