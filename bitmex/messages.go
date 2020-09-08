5c8306984e72965242970696386ab87f40cbda5c
Ilya Popovnin <itdbnice@gmail.com>
2020-09-08 12:20:20 +0300

    Use string constants as enum values in bitmex

--- bitmex/messages.go
+++ bitmex/messages.go
@@ -1,21 +1,19 @@
 package bitmex
 
-// TODO: unmarshal enum https://gist.github.com/lummie/7f5c237a17853c031a57277371528e87
-
-// BitmexMessage follows pattern {"op": "<command>", "args": ["arg1", "arg2", "arg3"]}
+// Message follows pattern {"op": "<command>", "args": ["arg1", "arg2", "arg3"]}
 type Message struct {
 	Op   string   `json:"op"`
 	Args []string `json:"args"`
 }
 
-// BitmexSubscriptionResultMessage ...
+// SubscriptionResultMessage ...
 type SubscriptionResultMessage struct {
 	Success   bool    `json:"success"`
 	Subscribe Pair    `json:"subscribe"`
 	Request   Message `json:"request"`
 }
 
-// BitmexSubscriptionMessage ...
+// SubscriptionMessage ...
 type SubscriptionMessage struct {
 	Table  string `json:"table"`
 	Action string `json:"action"` // enum [partial | insert | update | delete]
@@ -27,5 +25,15 @@ type SubscriptionMessage struct {
 		Price  float32 `json:"price"`
 	} `json:"data"`
 }
+
+const (
+	ActionPartial = "partial"
+	ActionInsert  = "insert"
+	ActionUpdate  = "update"
+	ActionDelete  = "delete"
+
+	SideBuy  = "Buy"
+	SideSell = "Sell"
+)
 
 type SubscriptionMessageHandler func(SubscriptionMessage)
